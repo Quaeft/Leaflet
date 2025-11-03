1cfea7be6d4e717474645497154a37f6378b2f23
Quaeft <andy@schellen.lu>
2025-11-03 01:09:00 +0100

    Revert "Remove scale control"
    This reverts commit 30bc0f75deb62cb549c4f3d4fd7da1a595bdf6a1.

--- src/control/index.js
+++ src/control/index.js
@@ -1,11 +1,14 @@
 import {Control, control} from './Control';
 import {Layers, layers} from './Control.Layers';
 import {Zoom, zoom} from './Control.Zoom';
+import {Scale, scale} from './Control.Scale';
 
 Control.Layers = Layers;
 Control.Zoom = Zoom;
+Control.Scale = Scale;
 
 control.layers = layers;
 control.zoom = zoom;
+control.scale = scale;
 
 export {Control, control};
