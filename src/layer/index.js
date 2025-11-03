b553f5840d00399dc760499d425f787aa1285eb6
Quaeft <andy@schellen.lu>
2025-11-03 01:10:39 +0100

    Revert "Remove SVGOverlay"
    This reverts commit dc20df72da1525e17168b77763329f98365ee314.

--- src/layer/index.js
+++ src/layer/index.js
@@ -12,6 +12,7 @@ GeoJSON.asFeature = asFeature;
 export {GeoJSON, geoJSON, geoJson};
 
 export {ImageOverlay, imageOverlay} from './ImageOverlay';
+export {SVGOverlay, svgOverlay} from './SVGOverlay';
 
 export {DivOverlay} from './DivOverlay';
 export {Popup, popup} from './Popup';
