b6d9a282ff49174224e29832f110a029a136155d
Quaeft <andy@schellen.lu>
2025-11-03 01:10:30 +0100

    Revert "Remove GeoJSON"
    This reverts commit f1eb95db1323d2a94fdf82b6c0a22530b31b0bf8.

--- spec/suites/layer/LayerGroupSpec.js
+++ spec/suites/layer/LayerGroupSpec.js
@@ -73,9 +73,33 @@
 		});
 	});
 
-	describe('#invoke', () => {
-		it('should invoke `setOpacity` method on every layer', () => {
-			const layers = [
+	describe("#toGeoJSON", function () {
+		it('should return valid GeoJSON for a layer with a FeatureCollection', function () {
+			var geoJSON = {
+				"type":"FeatureCollection",
+				"features":[
+					{
+						"type":"Feature",
+						"properties":{},
+						"geometry": {
+							"type":"Point",
+							"coordinates": [78.3984375, 56.9449741808516]
+						}
+					}
+				]
+			};
+
+			var layerGroup = L.layerGroup();
+			var layer = L.geoJSON(geoJSON);
+			layerGroup.addLayer(layer);
+
+			L.geoJson(layerGroup.toGeoJSON());
+		});
+	});
+
+	describe("#invoke", function () {
+		it('should invoke `setOpacity` method on every layer', function () {
+			var layers = [
 				L.marker([0, 0]),
 				L.marker([1, 1])
 			];
